1c2bf6d98e2142f65d7d36d492f61c0865cc588f
David Adelberg <davidtadelberg@gmail.com>
2025-12-07 11:03:16 -0500

    attempt to fix next export deprecation image optimization error on cloudflare

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: {
+    unoptimized: true,  // <-- Add this line
+  },
   // i18n: {
   //   locales: ['en'],
   //   defaultLocale: 'en',
