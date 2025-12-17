81b87ebbd4f5dee6f315c777732a106fe9b218d7
DGologorsky <71216283+DGologorsky@users.noreply.github.com>
2025-12-16 19:52:33 -0500

    Update Education.js

--- components/Education.js
+++ components/Education.js
@@ -1,300 +1,54 @@
+
+import { education, awards } from "../const/data";
+import { IoBookSharp } from "react-icons/io5";
+import { GiTrophyCup } from "react-icons/gi";
+import parse from 'html-react-parser';
+
 export default function Education() {
     return (
       <div>
-        <header>
-            <h2 className="h2 article-title">Education</h2>
-        </header>
-        <section className="colorlib-publications" id="publications">
-            <div className="colorlib-narrow-content">
-                <div className="row">
-                <div className="col-md-12">
-                    <h3 className="colorlib-heading">Selected PubMed Medical Manuscripts</h3>
-                </div>
-                </div>
-                <div className="col-md-12 mt-3">
-                <ol className="pub-list">
-                    <li>
-                    1. Osigian CJ, <strong>Gologorsky D</strong>, Cavuoto KM, Berrocal A,
-                    Villegas V. Oral aceta zolamide as a medical adjuvant to retinal surgery in optic disc
-                    pit maculopathy in a pediatric patient. Am J Ophthalmol Case Rep. 2020
-                    Jan 16;17:
-                    </li>
-                    <li>
-                    2. Kaplan RI, Drinkwater OJ, Lee RH, Chod RB, Barash A, Giovinazzo JV,
-                    <strong>Gologorsky D</strong> , Jansen ME, Rosen RB, Gentile RC. Pain
-                    Control after Intravitreal Injection Using Topical Nepa fenac 3% or Pressure Patching: A Randomized, Placebo-Controlled
-                    Trial. Ophthalmol Retina. 2019 Apr
-                    </li>
+        {/* <header>
+	        <h2 className="h2 article-title">Education</h2>
+        </header> */}
+        <h3 className="colorlib-heading mb-3">Education</h3>
+        <section className="timeline">
 
-                    <li>
-                    3. <strong>Gologorsky D</strong>. Eponymous Dishonor: Kyrielies Plaques.
-                    Retina. 2018 Jul;38(7):1261-
-                    </li>
-
-                    <li>
-                    4. <strong>Gologorsky D</strong> , Rosen R, Lee J. Navigated Retina Laser
-                    Therapy (Navilas) as a novel method of laser retinopexy for retinal
-                    tears. OSLI-Retina 2018 Nov 1;49(11):e206-e209
-                    </li>
-
-                    <li>
-                    5. Spierer O, <strong>Gologorsky D</strong> , Adler E, Forster RK.
-                    Lamellar keratoplasty with corneoscleral graft for limbal dermoids.
-                    Int J Ophthalmol. 2018 Mar 18;11(3):512-
-                    </li>
-
-                    <li>
-                    6. <strong>Gologorsky D</strong> , Spierer O. Asteroid Hyalosis. Eur J
-                    Intern Med, May 2017
-                    </li>
-
-                    <li>
-                    7. <strong>Gologorsky D</strong>, Williams BK, and Flynn HW Jr. Posterior
-                    Pole Retinal Detachment Due to a Macular Hole in a Patient with a
-                    Boston Keratoprosthesis. American Journal of Ophthalmology. 2016
-                    April; 5:56-58
-                    </li>
-
-                    <li>
-                    8. <strong>Gologorsky D</strong> and Flynn HW Jr. Cataract Surgery in the
-                    Setting of Severe Pathologic Myopia with High Axial Length: Use of
-                    Pars Plana Lensectomy and Vitrectomy. Clin Ophthalmol. 2016 May
-                    27;10:989-
-                    </li>
-
-                    <li>
-                    9. <strong>Gologorsky D</strong> , Gregori N, Goldhardt R. Dermatologic
-                    and Retinal Manifestations of Pseudoxanthoma Elasticum. Br J Dermatol.
-                    2016 Apr;174(4):e21
-                    </li>
-
-                    <li>
-                    10 .Sridhar J, Isom RF, <strong>Gologorsky D</strong> , Schiffman J,
-                    Ajuria L, Huang L , Banta JT. Utilization of Ophthalmology-Specific
-                    Emergency Department Services. Semin Ophthalmol. 2016 Sep 6:1- Gologorsky D
-                    </li>
-
-                    <li>
-                    11. Spierer O, Monsalve PF, O’Brien TP, Alfonso EC,
-                    <strong>Gologorsky D</strong> , Miller D. Clinical features,
-                    antibiotic
-                    </li>
-                    <li>
-                    12. susceptibility profiles, and outcomes of infectious keratitis caused
-                    by Achromobacter xylosoxidans. Cornea.2016 May;35(5):626-
-                    </li>
-
-                    <li>
-                    13. <strong>Gologorsky D</strong> . A Piece of my Mind. On Vision and
-                    Perception. JAMA. 2014 Feb 19;311(7):675-6
-                    </li>
-
-                    <li>
-                    14. <strong>Gologorsky D</strong> , Murray TG. Letter to the editor:
-                    immediate implications of improved surgical efficiency. Clin
-                    Ophthalmol. 2013;7:2287-8
-                    </li>
-
-                    <li>
-                    15. <strong>Gologorsky D</strong> , Chang JS, Hess DJ, Berrocal AM.
-                    Familial Exudative Vitreoretinopathy in a Premature Child. Ophthalmic
-                    Surgery, Lasers &amp; Imaging – Retina. 2013 Nov 1;44(6):603-5
-                    </li>
-
-                    <li>
-                    16. Brodt J, <strong>Gologorsky D</strong> , Walter S, Pham SM, Gologorsky
-                    E. Orbital Compression Syndrome Following Extracorporeal Support.
-                    Journal of Cardiac Surgery. 2013 Sep;28(5):522-4
-                    </li>
-
-                    <li>
-                    17. Schefler AC, <strong>Gologorsky D</strong> , Marr BP, Shields CL,
-                    Zeolite I, Abramson DH. Extraocular Metastases of Choroidal Melanoma
-                    After Invasive Diagnostic Evaluation. JAMA Ophthalmol. 2013
-                    Sep;131(9):1220-42
-                    </li>
-
-                    <li>
-                    18. <strong>Gologorsky D</strong> , Greenstein SH. Retrospective Analysis
-                    of Second Opinion Patients Self-Referred to Comprehensive
-                    Ophthalmology. (Journal of Clinical Ophthalmology. 2013 June.
-                    2013;7:1099 –
-                    </li>
-
-                    <li>
-                    19. <strong>Gologorsky D</strong>, Jakobiec FA, Freitag S.
-                    Transconjunctival Elimination of a Meibomian Keratinous Cyst.
-                    Ophthalmic Plastic and Reconstructive Surgery. Ophthalmic Plastic
-                    &amp; Reconstructive Surgery. 2012 Dec
-                    </li>
-
-                    <li>
-                    20. Hemmati HD, <strong>Gologorsky D</strong> , Pineda R. Intraoperative
-                    Wavefront Aberrometry in Cataract Surgery. Seminars in Ophthalmology.
-                    Seminars in Ophthalmology. 2012 Sep-Nov;27(5-6):100-
-                    </li>
-
-                    <li>
-                    21. Murray TG, Layton AJ, Tong KB, Gittelman M,
-                    <strong>Gologorsky D</strong> , Vigoda M. Transition to a novel
-                    advanced integrated vitrectomy platform: Comparison of surgical impact
-                    of moving from the Accurus to the Constellation Vision System. Journal
-                    of Clinical Ophthalmology. 2013;7:367-
-                    </li>
-
-                    <li>
-                    22. <strong>Gologorsky D</strong> , Thanos A, Vavvas D. Therapeutic
-                    Interventions against Inflammatory and Angiogenic Mediators in
-                    Proliferative Diabetic Retinopathy. Mediators of Inflammation. 2012;
-                    2012: 629452
-                    </li>
-
-                    <li>
-                    23. Bekelis K, Desai A, Zhao W, Gibson D, <strong>Gologorsky D</strong> ,
-                    Eskey C, Erkmen K. CT angiography: Improving diagnostic yield and
-                    cost-effectiveness in the initial evaluation of spontaneous
-                    non-subarachnoid intracerebral hemorrhage. Journal of Neurosurgery .
-                    2012 Oct;117(4):761-
-                    </li>
-
-                    <li>
-                    24. Khan S, Finger PT, Yu GP, Razzaq L, Jager MJ, de Keizer RJ, Sandkull
-                    P, Seregard S, <strong>Gologorsky D</strong> , Schefler AC, Murray TG,
-                    Kivelä T, Giuliari GP, McGowan H, Simpson ER, Corriveau C, Coupland
-                    SE, Damato BE. Angle involvement and glaucoma in patients with
-                    biopsy-proven iris melanoma: a response-reply. Archives of
-                    Ophthalmology. Archives of Ophthalmology. September 2012;130(9):1229-
-                    </li>
-
-                    <li>
-                    25. Khan S, Finger PT, Yu GP, Razzaq L, Jager MJ, de Keizer RJ, Sandkull
-                    P, Seregard S, <strong>Gologorsky D</strong> , Schefler AC, Murray TG,
-                    Kivelä T, Giuliari GP, McGowan H, Simpson ER, Corriveau C, Coupland
-                    SE, Damato BE. Clinical and pathologic characteristics of
-                    biopsy-proven iris melanoma: a multicenter international study.
-                    Archives of Ophthalmology. January 2012;130(1):57-
-                    </li>
-
-                    <li>
-                    26. <strong>Gologorsky D</strong> , Schefler AC, Williams BK, Dubovy SR,
-                    Murray TG. Medulloepithelioma: Invasive versus Noninvasive Diagnostic
-                    Methods and their Impacts on Outcome. Retinal Cases and Brief Reports,
-                    Winter
-                    </li>
-
-                    <li>
-                    27. Piña Y, Decatur C, Murray TG, Houston SK,
-                    <strong>Gologorsky D</strong> , Cavalcante M, Cavalcante L, Hernandez
-                    H, Celdran M, Feuer W, Lampidis T. Advanced retinoblastoma treatment:
-                    targeting hypoxia by inhibition of the mammalian target of rapamycin
-                    (mTOR) in LH BETA T AG retinal tumors. Journal of Clinical
-                    Ophthalmology. March 2011;5:337–
-                    </li>
-
-                    <li>
-                    28. Williams BK, Cebulla CM, Schefler AC, Fernandes CE,
-                    <strong>Gologorsky D</strong> , Murray TG. Required reduction in dose
-                    of chemotherapy for retinoblastoma due to Down syndrome-associated
-                    chemosensitivity. Retinal Cases and Brief Reports, Winter
-                    </li>
-
-                    <li>
-                    29. Williams BK Jr, Schefler AC, Garonzik SN,
-                    <strong>Gologorsky D</strong> , Shi W, Cavalcante LL, et al.
-                    Frequentprosthesis refitting to prevent implant exposure in patients
-                    with retinoblastoma. Journal of Pediatric Ophthalmology and
-                    Strabismus. 2010;23:1-
-                    </li>
-
-                    <li>
-                    30. <strong>Gologorsky D</strong> , Schefler AC, Ehlies FJ, Raskauskas PA,
-                    Williams BK, Murray TG. Clinical Imaging and High-Resolution
-                    Ultrasonography in Melanocytoma Management. Journal of Clinical
-                    Ophthalmology. 2010; 4: 855–
-                    </li>
-
-                    <li>
-                    31. Schefler AC, <strong>Gologorsky D</strong> , Murray TG. Primary
-                    Intraocular Lymphoma: An Update on Pathology and Treatments. Retina
-                    Today, August
-                    </li>
-
-                    <li>
-                    32. Gologorsky Y, <strong>Gologorsky D</strong> , Surti U, Yarigina A, MJ
-                    Zirwas. Karyotypic and Genetic Abnormalities in Familial Multiple
-                    Lipomatosis: A Case Report and Review of the Literature. Cutis. March
-                    2007; 79:227-
-                    </li>
-                </ol>
-                </div>
+            <div className="title-wrapper">
+            <div className="icon-box">
+                <IoBookSharp />
             </div>
-            </section>
-            <div className="col-md-12 mt-4 mb-3">
-			<h3 className="colorlib-heading">Selected Textbook Chapters</h3>
-		</div>
-		<div className="col-md-12">
-			<ol className="chapter-list">
-				<li>
-				1. Mehta N, <strong>Gologorsky D</strong> . Fluorescein Angiography&#34; In:
-				Retinal Imaging and Diagnostics. Lee J and Rosen RB (eds). Springer
-				International Publishing Switzerland,
-				</li>
 
-				<li>
-				2. <strong>Gologorsky D</strong> and Mohsenin A. &#34;Central Retinal Artery
-				Occlusion&#34; In: Manual of Retinal Disease. Medina CA, Townsend JH,
-				Singh AD (eds). Springer International Publishing Switzerland,
-				</li>
+            <h3 className="colorlib-heading">Education & Training</h3>
+            </div>
 
-				<li>
-				3. <strong>Gologorsky D</strong> and Mohsenin A. &#34;Branch Retinal Artery
-				Occlusion&#34; In: Manual of Retinal Disease. Medina CA, Townsend JH,
-				Singh AD (eds). Springer International Publishing Switzerland,
-				</li>
+            <ol className="timeline-list">
+                { education.map((item,index) => <li className="timeline-item" key={index}>
+                    <h4 className="h4 timeline-item-title">{item.college}</h4>
+                    <p className="timeline-text"> {parse(item.study)} </p></li>)
+                }
+            </ol>
 
-				<li>
-				4. <strong>Gologorsky D</strong> and Mohsenin A. &#34;Cilioretinal Artery
-				Occlusion&#34; In: Manual of Retinal Disease . Medina CA, Townsend JH,
-				Singh AD (eds). Springer International Publishing Switzerland,
-				</li>
+        </section>
 
-				<li>
-				5. <strong>Gologorsky D</strong> and Mohsenin A. &#34;Idiopathic Juxtafoveal
-				Telangiectasias&#34; In: Manual of Retinal Disease. Medina CA, Townsend
-				JH, Singh AD (eds). Springer International Publishing Switzerland,
-				</li>
+        <section className="timeline">
 
-				<li>
-				6. <strong>Gologorsky D</strong> and Mohsenin A. &#34;Ocular Ischemic
-				Syndrome&#34; In: Manual of Retinal Disease . Medina CA, Townsend JH,
-				Singh AD (eds). Springer International Publishing Switzerland,
-				</li>
+            <div className="title-wrapper">
+            <div className="icon-box">
+                <GiTrophyCup />
 
-				<li>
-				7. <strong>Gologorsky D</strong> and TG Murray . &#34;Enucleation surgery in
-				advanced ocular disease&#34; In: Ocular Melanoma: Advances in Diagnostic
-				and Therapeutic Strategies . Murray TG and H Culver Boldt (eds). 2013
-				</li>
+            </div>
 
-				<li>
-				8. <strong>Gologorsky D</strong> . &#34;The Cornea. In: Fundamentals of Basic
-				Science and Clinical Medicine. First edition. Providence, RI:
-				Medectomy Publishing;
-				</li>
+            <h3 className="colorlib-heading">Awards</h3>
+            </div>
 
-				<li>
-				9. <strong>Gologorsky D</strong> . The Lens. In: Fundamentals of Basic
-				Science and Clinical Medicine. First edition. Providence, RI:
-				Medectomy Publishing;
-				</li>
+            <ol className="timeline-list">
+                { awards.map((item,index) => <li className="timeline-item" key={index}>
+                    <h4 className="h4 timeline-item-title">{item.name}</h4>
+                    <p className="timeline-text"> {item.desc} </p></li>)
+                }
+            </ol>
 
-				<li>
-				10. <strong>Gologorsky D</strong> and AC Schefler. Ocular Oncology. In:
-				Fundamentals of Basic Science and Clinical Medicine. First edition.
-				Providence, RI: Medectomy Publishing;
-				</li>
-			</ol>
-		</div> 
+        </section>
       </div>
     
   )
